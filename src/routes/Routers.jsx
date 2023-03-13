9f21d75b8c50359eb91ec3fe8511004ebca0f325
Nabil GAYL <gaylnabil@gmail.com>
2023-03-13 15:49:19 +0100

    delete comments in Routes file

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -37,7 +37,12 @@ const Routers = () => {
                     ></Route> 
     */}
         </Routes>
-    )
+ /** 
+* Brief description of the function here.
+* @summary If the description is long, write your summary here. Otherwise, feel free to remove this.
+* @param {ParamDataTypeHere} parameterNameHere - Brief description of the parameter here. Note: For other notations of data types, please refer to JSDocs: DataTypes command.
+* @return {ReturnValueDataTypeHere} Brief description of the returning value here.
+*/   )
 }
 
 export default Routers
