513f48e825c0c9f3ae2d6f07c0e452dd69f9a7e3
Nabil GAYL <gaylnabil@gmail.com>
2023-03-13 15:56:48 +0100

    adding comments in store and CounterSlice js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import { counterReducer } from "./slices/CounterSlice";
 import { productReducer } from "./slices/ProductSlice";
 
+// Configure and return service decorator store. This is a wrapper around configureStore ()
+
 export const store = configureStore({
   reducer: {
     counterStore: counterReducer,
